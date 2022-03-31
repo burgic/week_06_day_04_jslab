9a37f65ec7cc96dc6d5c6116f991359fac673967
burgic <ceburgin@gmail.com>
2022-03-31 15:33:03 +0100

    part finished lab

--- js/app.js
+++ js/app.js
@@ -1,3 +1,27 @@
 document.addEventListener('DOMContentLoaded', () => {
   console.log('JavaScript loaded');
-})
+
+  const form = document.querySelector('#new-item-form');
+  form.addEventListener('submit', handleFormSubmit);
+
+});
+
+
+const handleFormSubmit = function(event) {
+  event.preventDefault();
+  const resultParagraph = document.querySelector('.form-item')
+  resultParagraph.textContent = `Your book ${event.target.title.value}`
+  
+
+const newListItem = document.createElement('reading-list')
+newListItem.textContent = `Book: ${event.target.title.value} ${event.target.author.value} ${event.target.category.value}`;
+newListItem.classList.add
+const list = document.querySelector('ul')
+list.appendChild(newListItem)
+
+document.getElementById("new-item-form").reset()
+
+console.log(event.target.title.value)
+console.log(event.target.author.value)
+console.log(event.target.category.value)
+}
